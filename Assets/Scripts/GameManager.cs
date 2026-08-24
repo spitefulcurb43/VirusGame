793b1b8350f74b98b655611d49bdb7352f1b851f
spitefulcurb46 <freddiepell7@gmail.com>
2026-08-24 14:15:18 +0100

    GameManager Edits
    This is the last thing i do for today gl gng

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    //Enum Stuff
     public enum GameStates { Intro, Playable, Win, Lose }; //Game States, starts at Intro. (I don't think we need a menu screen)
     public GameStates gameState;
 
 
-    //This should be useful.
-    public Transform cursor;
+
+    //Relevant variables
+    public int hp = 100; //How many files you have left.
+    public int progress = 0; //How complete the antivirus installation is.
+
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        //Initialise cursor
         cursor = transform.Find("Cursor");
     }
 
@@ -24,10 +31,33 @@ public class GameManager : MonoBehaviour
         DoCursor();
     }
 
-
+    //Cursor
+    public Transform cursor;
     private void DoCursor()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         cursor.position = mousePos;
     }
+
+
+
+    /*This is a struct I made to store some simple info about each minigame. 
+    With this we could:
+    - Load specific minigames
+    - Load a minigame of a certain difficulty
+    - Keep track of which minigames have previously spawned.
+
+    (don't worry if you don't get how to add stuff to it, just ask me(freddie)!!!)
+    */
+
+    public MinigameInfo[] minigameInfos;
+
+    [Serializable]
+    public struct MinigameInfo
+    {
+        public string name;
+        public Difficulty difficulty;
+        public GameObject prefab;
+    }
+    public enum Difficulty { easy, medium, hard};
 }
