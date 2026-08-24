23af578774265286dd4a0b3fd611691275600cb2
spitefulcurb46 <freddiepell7@gmail.com>
2026-08-24 12:35:52 +0100

    Movable, Closable PopUps
    Working!!!!!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,15 +8,26 @@ public class GameManager : MonoBehaviour
     public enum GameStates { Intro, Playable, Win, Lose }; //Game States, starts at Intro. (I don't think we need a menu screen)
     public GameStates gameState;
 
+
+    //This should be useful.
+    public Transform cursor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cursor = transform.Find("Cursor");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DoCursor();
+    }
+
+
+    private void DoCursor()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursor.position = mousePos;
     }
 }
