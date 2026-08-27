5e47ba81c140406c6dcc3c43b4bf77e61ec38058
spitefulcurb46 <freddiepell7@gmail.com>
2026-08-27 17:34:30 +0100

    Added Lock Screen
    bottom buttons will exist soon

---
+++ Assets/Scripts/MenuScreen.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScreen : MonoBehaviour
+{
+    public GameObject dotPrefab;
+    public Transform dotParent;
+
+    public string password;
+    public string correctPassword = "password";
+
+    public int maxPasswordLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (char c in Input.inputString)
+        {
+            if(c == '\b') //backspace pressed
+            {
+                //do backspace
+                if(password.Length > 0)
+                {
+                    Destroy(dotParent.GetChild(dotParent.childCount - 1).gameObject); //remove last kid
+                    password = password.Substring(0, password.Length - 1); //remove 1 from password
+                }
+            }
+            else if ((c == '\n') || (c == '\r')) //enter
+            {
+                CheckPassword();
+            }
+            else if (password.Length <= maxPasswordLength)
+            {
+                Instantiate(dotPrefab, Vector3.zero, Quaternion.identity, dotParent); //Add a dot.
+                password += c; //Add character to password.
+            }
+        }
+    }
+
+    public void CheckPassword()
+    {
+        if (password == correctPassword)
+        {
+            print("UNLOCKED! " + password);
+            transform.gameObject.SetActive(false);
+        }
+        else
+        {
+            print("incorrect " + password);
+            foreach (Transform kid in dotParent) Destroy(kid.gameObject);
+            password = "";
+        }
+    }
+}
